333ecd39c7281e2fe514dc2f8bc99379f3926eda
Aryan Kumar Dubey <96795631+Aryan-we@users.noreply.github.com>
2024-09-29 18:52:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+let ingredients=document.getElementById("ingredients");
+ingredients.addEventListener("click",function(){
+    
+    if(document.getElementById("ingredients").innerText=="Show Ingredients"){
+        document.getElementById("ingredients").innerText="Hide Ingredients";
+        document.getElementsByClassName("ingredients")[0].style.display="block";
+    }else if(document.getElementById("ingredients").innerText=="Hide Ingredients"){
+        document.getElementById("ingredients").innerText="Show Ingredients";
+        document.getElementsByClassName("ingredients")[0].style.display="none";
+        
+    }
+    
+})
+let cooking=document.getElementById("startCooking");
+
+
+let cooking_list=document.getElementsByClassName("instructions-list")[0].querySelectorAll("li");
+for(let i=0;i<cooking_list.length;i++){
+    cooking_list[i].style.display="none";
+   }
+   
+cooking.addEventListener("click",function(){
+    document.getElementById("next_btn").style.display="inline";
+    document.getElementsByClassName("instructions")[0].querySelector("h2").style.display="block";
+
+
+    if(document.getElementById("startCooking").innerText=="Start Cooking"){
+    let next_btn=document.getElementById("next_btn");
+    let current_index=0;
+    cooking_list[0].style.display="block";
+   next_btn.addEventListener("click",function(){
+        
+    cooking_list[current_index].style.display="none";
+    current_index=current_index+1;
+    if(current_index>=cooking_list.length){
+            current_index=0;
+        
+            }
+            cooking_list[current_index].style.display="block";
+        
+    })
+    document.getElementById("startCooking").style.display="none";
+    }
+    
+})
